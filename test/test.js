15fdf222f89412f9a563af3cfe2dbbb7133efa30
stereobit <t@stereob.it>
2013-09-08 16:43:19 +0100

    add dragend to tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
-describe('Array', function(){
-  describe('#indexOf()', function(){
+describe('Instantiation', function() {
+  before();
+  describe('Settings', function() {
     it('should return -1 when the value is not present', function(){
       expect([1,2,3].indexOf(5)).to.be(-1);
       expect([1,2,3].indexOf(0)).to.be(-1);
