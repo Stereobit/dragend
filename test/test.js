595bb140ebca6a25e4b49609b6343372dbf87524
stereobit <t@stereob.it>
2013-09-08 16:25:39 +0100

    tests working

--- test/test.js
+++ test/test.js
@@ -1,10 +1,8 @@
-var assert = require("assert");
-
-describe('Array', function() {
+describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      expect([1,2,3].indexOf(5)).to.be(-1);
+      expect([1,2,3].indexOf(0)).to.be(-1);
     });
   });
 });
