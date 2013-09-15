99fb409dec47168528493de9997bb5532b1d2bc4
stereobit <t@stereob.it>
2013-09-15 14:46:03 +0100

    rase version

--- dragend.js
+++ dragend.js
@@ -1,7 +1,7 @@
 /**
  * ---------------------------- DRAGEND JS -------------------------------------
  *
- * Version: 0.1.3
+ * Version: 0.2.0_rc1
  * https://github.com/Stereobit/dragend
  * Copyright (c) 2012 Tobias Otte, t@stereob.it
  *
