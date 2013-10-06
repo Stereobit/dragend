44d3073518060a171392298f9df9059a0451cc4a
stereobit <t@stereob.it>
2013-10-06 18:08:38 +0100

    fix mocha tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
+var expect = require('expect.js');
+
 describe('Instantiation', function() {
-  before();
   describe('Settings', function() {
-    it('should return -1 when the value is not present', function(){
+    it('should return -1 when the value is not present', function() {
       expect([1,2,3].indexOf(5)).to.be(-1);
       expect([1,2,3].indexOf(0)).to.be(-1);
     });
