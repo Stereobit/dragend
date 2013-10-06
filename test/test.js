cb9d581aa4748077fa2b62e5175c2a0ef159a49a
stereobit <t@stereob.it>
2013-10-06 22:45:33 +0100

    test jquery integration

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ function createDom() {
   var dom = $("<div><div class='dragend-page'></div><div class='dragend-page'></div><div class='dragend-page'></div></div>");
 
   return dom.get(0);
-};
+}
 
 describe('instantiation', function() {
   describe('settings', function() {
@@ -22,6 +22,8 @@ describe('instantiation', function() {
         onDrag: function() {}
       });
 
+      jQueryDragend = $(createDom()).dragend();
+
     });
 
     it('check if settings are inherited', function() {
@@ -32,6 +34,10 @@ describe('instantiation', function() {
     it('check if settings updated', function() {
       expect(updateInstanceDragend.settings.duration).to.be(14);
       expect(updateInstanceDragend.settings.onDrag).to.be.a('function');
+    });
+
+    it('check if jquery creates instance', function() {
+      expect(jQueryDragend.data('dragend')).to.be.an(Dragend);
     });
 
   });
