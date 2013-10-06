0a38d92a5cab0629cfbdf7139a2a793e2ab0f5c3
stereobit <t@stereob.it>
2013-10-06 22:17:15 +0100

    update tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
-var expect = require('expect.js');
-
 describe('Instantiation', function() {
   describe('Settings', function() {
+
+    before(function(){
+      dragend = new Dragend($('#dragend').get(0));
+    });
+
     it('should return -1 when the value is not present', function() {
+      console.log(dragend)
       expect([1,2,3].indexOf(5)).to.be(-1);
       expect([1,2,3].indexOf(0)).to.be(-1);
     });
+
   });
 });
