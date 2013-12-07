c79bdc96739587dad6e9680ff44ec83a7bd17f6b
stereobit <t@stereob.it>
2013-12-07 18:56:25 +0000

    some more tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,25 @@
-function createDom() {
-  var dom = $("<div><div class='dragend-page'></div><div class='dragend-page'></div><div class='dragend-page'></div></div>");
+var domCache = [];
 
+function createDom() {
+  var dom = $("<div style='width: 100px; height: 100px;'><div class='dragend-page'></div><div class='dragend-page'></div><div class='dragend-page'></div></div>");
+  $(document.body).append(dom);
+  domCache.push(dom);
   return dom.get(0);
+}
+
+function clearDom() {
+  $.each(domCache, function(index, element) {
+    element.remove();
+  });
 }
 
 describe('instantiation', function() {
   describe('settings', function() {
     var inheritedDragend,
-        updateInstanceDragend;
+        updateInstanceDragend,
+        jQueryDragend
 
-    before(function(){
+    before(function(done){
 
       inheritedDragend = new Dragend(createDom(), {
         duration: 14,
@@ -24,7 +34,11 @@ describe('instantiation', function() {
 
       jQueryDragend = $(createDom()).dragend();
 
+      window.setTimeout(done, 20);
+
     });
+
+    after(clearDom);
 
     it('check if settings are inherited', function() {
       expect(inheritedDragend.settings.duration).to.be(14);
@@ -42,6 +56,25 @@ describe('instantiation', function() {
 
     it('check if content gets copied into new container', function() {
       expect(inheritedDragend.container.childNodes[0].className).not.to.equal('dragend-page');
+    });
+
+
+  });
+
+  describe('create pages and sizing', function() {
+    var instance;
+
+    before(function(done) {
+      instance = new Dragend(createDom());
+      window.setTimeout(done, 20);
+    });
+
+    it('check if pages size got set right', function() {
+      expect(instance.container.childNodes[0].childNodes[0].offsetWidth).to.eql(instance.container.offsetWidth);
+    });
+
+    it('check if pages are found', function() {
+      expect(instance.pages.length).to.be(3);
     });
 
   });
