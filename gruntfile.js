393961805a0753fc1ea7adeabc7c47eb718addd6
Tobias Otte <t@stereob.it>
2019-02-03 18:39:13 +0000

    remove test coverage

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,5 @@
 module.exports = function (grunt) {
 
-    var istanbul = require('istanbul');
-
     // grunt plugins
     require('load-grunt-tasks')(grunt);
 
@@ -25,38 +23,6 @@ module.exports = function (grunt) {
                 template: 'test/index.template.html',
                 runner: 'test/index.html',
                 files: 'test/spec/**/*'
-            }
-        },
-
-        coverage: {
-
-            // when the coverage object is received
-            // from grunt-mocha it will be saved here
-            coverage: null,
-
-            instrument: {
-
-                // files to NOT instrument eg. libs
-                // these files will be copied "as is"
-                ignore: [
-                    'src/js/lib/**/*'
-                ],
-
-                // files to instrument
-                files: [
-                    {
-                        src: 'dragend.js',
-                        expand: true,
-                        cwd: 'dist',
-                        dest: 'test/src'
-                    }
-                ]
-            },
-
-            // task for generating reports
-            report: {
-                reports: ['html', 'text-summary'],
-                dest: 'test/reports'
             }
         },
 
@@ -103,61 +69,6 @@ module.exports = function (grunt) {
 
     });
 
-    grunt.event.on('coverage', function (coverage) {
-        grunt.config('coverage.coverage', coverage);
-    });
-
-    grunt.registerMultiTask('coverage', 'Generates coverage reports for JS using Istanbul', function () {
-
-        if (this.target === 'instrument') {
-
-            var ignore = this.data.ignore || [];
-            var instrumenter = new istanbul.Instrumenter();
-
-            this.files.forEach(function (file) {
-
-                var src = file.src[0],
-                    instrumented = grunt.file.read(src);
-
-                // only instrument this file if it is not in ignored list
-                if (!grunt.file.isMatch(ignore, src)) {
-                    instrumented = instrumenter.instrumentSync(instrumented, src);
-                }
-
-                // write
-                grunt.file.write(file.dest, instrumented);
-            });
-
-            return;
-        }
-
-        if (this.target === 'report') {
-
-            this.requiresConfig('coverage.coverage');
-
-            var Report = istanbul.Report;
-            var Collector = istanbul.Collector;
-            var reporters = this.data.reports;
-            var dest = this.data.dest;
-            var collector = new Collector();
-
-            // fetch the coverage object we saved earlier
-            collector.add(grunt.config('coverage.coverage'));
-
-            reporters.forEach(function (reporter) {
-
-                Report.create(reporter, {
-                    dir: dest + '/' + reporter
-                }).writeReport(collector, true);
-
-            });
-
-            return;
-        }
-
-        grunt.warn('Unknown target - valid targets are "instrument" and "report"');
-    });
-
     grunt.registerTask('test', 'Run JS Unit tests', function () {
 
         var options = this.options();
@@ -172,7 +83,8 @@ module.exports = function (grunt) {
 
         // write template to tests directory and run tests
         grunt.file.write(options.runner, template);
-        grunt.task.run('coverage:instrument', 'mocha', 'coverage:report');
+
+        grunt.task.run('mocha');
     });
 
     grunt.registerTask('build', 'build release versions', function () {
